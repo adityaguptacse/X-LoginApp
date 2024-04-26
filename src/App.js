9bac70653dca8934e2e09c954fcc8d9683ff3643
Aditya Gupta <adityaguptacse@gmail.com>
2024-04-25 19:06:28 -0700

    Login App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Welcome from "./Welcome";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    if(username === "user" && password === "password" ) {
+      setError("");
+      setIsSubmitted(true)
+    }
+    else{
+      setError("Invalid username or password")
+      setIsSubmitted(false)
+    }
+    
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login Page</h1>
+      {isSubmitted? (
+        <div>
+          <p>Welcome, {username}</p>
+        </div>
+      ): (
+        <form onSubmit={handleSubmit}>
+          {error && <p>{error}</p>}
+          <div>
+            <label htmlFor="username">Username</label>
+            <input 
+            type="text"
+            value={username}
+            id="username"
+            placeholder="username"
+            onChange={(e)=>{
+              setUsername(e.target.value)
+            }}
+            required
+            />
+            <label htmlFor="password">Password</label>
+            <input 
+            type="text"
+            value={password}
+            id="username"
+            placeholder="password"
+            onChange={(e)=>{
+              setPassword(e.target.value)
+            }}
+            required
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+
+        </form>
+      )}
     </div>
   );
 }
---
+++ src/Welcome.js
@@ -0,0 +1,7 @@
+export default function Welcome() {
+    return(
+        <dov>
+            <p>Welcome, User!</p>
+        </dov>
+    )
+}
